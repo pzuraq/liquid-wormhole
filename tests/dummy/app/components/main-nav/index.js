cd60296be0723c652f3db8e1129bdeac4fe20a04
Robert Wagner <rwwagner90@gmail.com>
2022-01-20 20:43:34 -0500

    Revert "Try updating closest"
    This reverts commit a469d9b1c0e1460447ee9428ece4877a0c753afa.

--- tests/dummy/app/components/main-nav/index.js
+++ tests/dummy/app/components/main-nav/index.js
@@ -9,7 +9,7 @@ export default class MainNav extends Component {
     const target = event?.target;
 
     if (target !== this.element) {
-      if (target.closest('a.nav-item')) {
+      if (target.closest(this.element.querySelector('a.nav-item')).length) {
         this.navOpen = false;
       }
     }
