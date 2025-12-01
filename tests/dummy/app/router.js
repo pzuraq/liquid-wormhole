cbf5f0c0c2d1a30c0bc7a8932cc375e4a793fd8f
Robert Wagner <rwwagner90@gmail.com>
2025-11-30 22:34:34 -0500

    Fix router

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,4 +6,21 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('demo');
+  this.route('docs', function () {
+    this.route('parameters');
+    this.route('stacks');
+    this.route('destinations');
+    this.route('components');
+  });
+  this.route('upgrading');
+  this.route('scenarios', function () {
+    this.route('manual-destinations');
+    this.route('nested-wormholes');
+    this.route('component-in-wormhole');
+    this.route('actions-in-wormhole');
+    this.route('liquid-fire-in-wormhole');
+    this.route('password-input-child');
+  });
+});
